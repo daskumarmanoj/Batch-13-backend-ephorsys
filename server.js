c4f742b9b078b022219d26e1bf86abaaa12918a2
Manoja Kumar Das <manoja.ephorys@gmail.com>
2026-06-23 13:05:22 +0530

    hii

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 import express from "express";
 import connectDB from "./db.js";
 import dns from "dns";
 import cookieParser from "cookie-parser";
 import UserRouter from "./routes/user.routes.js";
+import FoodRoutes from "./routes/food.routes.js";
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", UserRouter);
+app.use("/api/food", FoodRoutes);
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const PORT = process.env.PORT || 5500;
